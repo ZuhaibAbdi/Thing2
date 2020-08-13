20b8b31b425dc3f0f25abf3c5338616687af06ad
Coding <coding@Zuhaib.local>
2020-08-13 13:12:57 -0600

    hu

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-  
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+
+    autoplay: {
+        delay: 3000,
+    },
+    width: 600,
+
 })
