230aa6132de38e2c4e17c6605a47aa3a2a52de84
ZakariaAbd <65509346+ZakariaAbd@users.noreply.github.com>
2020-08-13 13:12:02 -0600

    jhkzsg

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   direction: 'vertical',
   loop: true,
-  
+
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  loop: true;
 })
